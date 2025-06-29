01417c3d4e55e0860e52bd81d5c0de6f65996c6c
taehyuek <matthew.lee.1185@gmail.com>
2025-06-29 16:41:47 +0900

    BeakJoon 1032 Edit

--- C++/1032/1032.cpp
+++ C++/1032/1032.cpp
@@ -11,18 +11,16 @@ int main(){
     for(int i=0; i<cnt; i++){
         cin >> input[i];
     }
+    input[cnt] = string(input[0].size(), '?');
     for(int i=0; i<input[0].size(); i++){
         tmp = input[0][i];
         for(int j=0; j<cnt; j++){
             if(tmp != input[j][i]){
-                input[cnt][i] = '?';
-                cout << input[cnt][i];
                 break;
             }
             tmp = input[j][i];
             if(j == cnt-1){
                 input[cnt][i] = tmp;
-                cout << input[cnt][i];
             }
         }
     }
