2f96a71f4643e28ff582180c521c9e53f5804687
taehyuek <matthew.lee.1185@gmail.com>
2025-07-09 16:34:17 +0900

    BaekJoon 3273

--- C++/3273/3273.cpp
+++ C++/3273/3273.cpp
@@ -6,7 +6,7 @@ int main(){
 	ios::sync_with_stdio(0);
 
 	int n, x, cnt = 0;
-	vector<bool> v(10000001);
+	vector<bool> v(1000001);
 	cin >> n;
 	for(int i=0; i<n; i++){
 		int tmp;
@@ -14,7 +14,7 @@ int main(){
 		v[tmp] = true;
 	}
 	cin >> x;
-	for(int i=0; i<x/2; i++){
+	for(int i=1; i<((x%2==0) ? x/2 : x/2+1); i++){
 		if(v[i]&&v[x-i]) cnt++;
 	}
 	cout << cnt;
